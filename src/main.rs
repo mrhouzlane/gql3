280ce0abf56e2a60d456abc6094885ebf9183c25
Mehdi Rhouzlane <rhouzm@gmail.com>
2023-04-19 05:27:30 +0800

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,64 @@
-#![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
+use reqwest;
+use serde::Deserialize;
+use serde_json::json;
 
-use graphql_client::{GraphQLQuery, Response};
-use serde_derive::{Deserialize, Serialize};
-use anyhow::anyhow;
-use anyhow::Error;
-use anyhow::Result as AnyhowResult;
-
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-#[serde(rename_all = "snake_case")]
-struct Escrows(String);
-
-
-#[derive(Serialize, Deserialize, Debug)]
-struct LaunchedEscrow {
-    id: Escrows,
+#[derive(Debug, Deserialize)]
+struct QueryResponse {
+    data: Option<Data>,
+    errors: Option<Vec<Error>>,
 }
 
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "src/schema.graphql",
-    query_path = "src/query.graphql",
-    response_derives = "Debug, Serialize, Deserialize"
-)]
-pub struct MyQuery;
+#[derive(Debug, Deserialize)]
+struct Data {
+    launchedEscrows: Vec<LaunchedEscrow>,
+}
 
-async fn perform_my_query(variables: my_query::Variables,
-) -> Result<Response<my_query::ResponseData>, Error> {
-    let request_body = MyQuery::build_query(variables);
-    let client = reqwest::Client::new();
-    let res = client.post("https://api.thegraph.com/subgraphs/name/humanprotocol/mumbai-v1")
-    .json(&request_body)
-    .send()
-    .await?;
-    let response_body: Response<my_query::ResponseData> =       res.json().await?;
-    Ok(response_body)
+#[derive(Debug, Deserialize)]
+struct LaunchedEscrow {
+    id: String,
+    token: String,
+    from: String,
+    timestamp: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Error {
+    message: String,
 }
 
 #[tokio::main]
-async fn main() -> AnyhowResult<(), Error> {
-    let variables = my_query::Variables;
-    let escrows_list = perform_my_query(variables)
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let query = r#"
+        {
+            launchedEscrows(first: 5) {
+                id
+                token
+                from
+                timestamp
+            }
+        }
+    "#;
+
+    let client = reqwest::Client::new();
+    let res = client
+        .post("https://api.thegraph.com/subgraphs/name/humanprotocol/mumbai-v1")
+        .json(&json!({
+            "query": query,
+        }))
+        .send()
         .await?
-        .data
-        .ok_or(anyhow!("Query failed"))?
-        .launched_escrows;
-    let mut escrows: Vec<Escrows> = Vec::new();
-    for raw_escrow in escrows_list {
-        let escrow_value = serde_json::to_value(raw_escrow).expect("Failed converting raw escrow to json value");
-        let escrow: LaunchedEscrow = serde_json::from_value(escrow_value)
-        .expect("Failed converting json value to escrow object");
-        escrows.push(escrow.id);
+        .json::<QueryResponse>()
+        .await?;
+
+    if let Some(errors) = res.errors {
+        for error in errors {
+            println!("Error: {}", error.message);
+        }
+    } else if let Some(data) = res.data {
+        println!("{:#?}", data.launchedEscrows);
+    } else {
+        println!("Response did not contain data or errors");
     }
-    println!("{:?}", escrows);
+
     Ok(())
 }
